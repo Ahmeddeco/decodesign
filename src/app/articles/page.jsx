1f60e41cdb331eed4758318134c2dd78b8b4267d
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-06-11 22:23:17 +0300

    Under developed

--- src/app/articles/page.jsx
+++ src/app/articles/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const ArticlesPage = () => {
 	return (
-		<section className='h1 h-screen text-base-100 '>ArticlesPage</section>
+		<section className=' h-screen text-base-100 flex-col gap-24'>
+			<h1 className='h1'>Articles Page</h1>
+			<h2 className='h2'>Under Developed</h2>
+		</section>
 	)
 }
 
