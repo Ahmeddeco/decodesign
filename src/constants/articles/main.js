7c50b701da7805dcb824ef07284d6e64b037ccc8
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-06-12 15:38:56 +0300

    article source ai

---
+++ src/constants/articles/main.js
@@ -0,0 +1,85 @@
+export const name_is = {
+	title: '',
+	mainParagraph: '',
+	points: [
+		{
+			id: 1,
+			title: '',
+			body: '',
+		},
+		{
+			id: 2,
+			title: '',
+			body: '',
+		},
+		{
+			id: 3,
+			title: '',
+			body: '',
+		},
+		{
+			id: 4,
+			title: '',
+			body: '',
+		},
+		{
+			id: 5,
+			title: '',
+			body: '',
+		},
+		{
+			id: 6,
+			title: '',
+			body: '',
+		},
+		{
+			id: 7,
+			title: '',
+			body: '',
+		},
+		{
+			id: 8,
+			title: '',
+			body: '',
+		},
+		{
+			id: 9,
+			title: '',
+			body: '',
+		},
+		{
+			id: 10,
+			title: '',
+			body: '',
+		},
+		{
+			id: 11,
+			title: '',
+			body: '',
+		},
+		{
+			id: 12,
+			title: '',
+			body: '',
+		},
+	],
+	endParagraph: '',
+	mainSources: [
+		{
+			name: '',
+			url: '',
+		},
+		{
+			name: '',
+			url: '',
+		},
+		{
+			name: '',
+			url: '',
+		},
+		{
+			name: '',
+			url: '',
+		},
+	],
+}
