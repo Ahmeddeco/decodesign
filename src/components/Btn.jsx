25fb894c04bfcf3408d941067923246f8b30d57f
ahmed <119008954+Ahmeddeco@users.noreply.github.com>
2024-08-31 14:28:15 +0300

    fix btn & product spacing

--- src/components/Btn.jsx
+++ src/components/Btn.jsx
@@ -3,14 +3,12 @@ import React from 'react'
 
 const Btn = ({ title, link }) => {
 	return (
-		<button className='btn-secondary btn-outline btn'>
-			<Link
-				href={link}
-				className='text-xl'
-			>
-				{title}
-			</Link>
-		</button>
+		<Link
+			href={link}
+			className='text-xl'
+		>
+			<button className='btn-secondary btn-outline btn'>{title}</button>
+		</Link>
 	)
 }
 
--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -12,23 +12,17 @@ const ProductCard = () => {
 						className=' '
 						key={id}
 					>
+						{/* main map container */}
 						<div
-							className={`flex lg:even:flex-row-reverse ${
-								id % 2 === 0
-									? 'lg:flex-row-reverse'
-									: 'lg:flex-row'
-							} flex-col items-center justify-between w-full min-h-screen lg:gap-16 gap-8`}
+							className={`flex  ${
+								id % 2 === 0 ? 'lg:flex-row-reverse' : 'lg:flex-row'
+							} flex-col items-center justify-center w-full  min-h-screen lg:gap-16 gap-8`}
 						>
-							{/* main map container */}
-
+							{/* text container */}
 							<div className='w-full lg:w-1/3 flex flex-col gap-4 lg:gap-12 '>
-								<h2 className='h2 text-secondary font-lemonada'>
-									{title}
-								</h2>
+								<h2 className='h2 text-secondary font-lemonada'>{title}</h2>
 								<div className='flex flex-col gap-4'>
-									<h5 className='h5 text-secondary'>
-										{location}
-									</h5>
+									<h5 className='h5 text-secondary'>{location}</h5>
 									<p className='p'>{paragraph}</p>
 								</div>
 								<Btn
@@ -37,15 +31,15 @@ const ProductCard = () => {
 									className='w-9'
 								/>
 							</div>
-							{/* images */}
-							<div className='w-full lg:w-2/3 flex lg:gap-16 gap-8 lg:flex-row flex-col  '>
+							{/* images container */}
+							<div className='w-full lg:w-fit flex lg:gap-16 gap-8 lg:flex-row flex-col  '>
 								<div className=' '>
 									<Image
 										src={pic[0].img.src}
 										alt={pic[0].img.alt}
 									/>
 								</div>
-								<div className=' w-full lg:w-1/2 flex flex-col lg:gap-16 gap-8 justify-between '>
+								<div className='  lg:w-1/2 flex flex-col lg:gap-16 gap-8 justify-between '>
 									<Image
 										src={pic[1].img.src}
 										alt={pic[1].img.alt}
